7322dcda32158a745f5d23cf5a510f6045a33cdf
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-03 02:14:46 +0000

    added a route for specific urls

--- express_server.js
+++ express_server.js
@@ -5,8 +5,25 @@ const PORT = 8080; // default port
 // set ejs as view engine
 app.set("view engine", "ejs");
 
+// create URL database
+const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  };
+
 // make a route for "/urls"
 app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+  });
+
+// make a route for "urls_show"
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render("urls_show", templateVars);
 });
+
+//listen to port 8080
+app.listen(8080);
+console.log(`app listening on port ${PORT}`);
