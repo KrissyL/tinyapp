6b688c4d4fa1ab4a77be308a01e7d9811362b420
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-03 00:58:33 +0000

    added route for '/urls'

--- express_server.js
+++ express_server.js
@@ -4,3 +4,9 @@ const PORT = 8080; // default port
 
 // set ejs as view engine
 app.set("view engine", "ejs");
+
+// make a route for "/urls"
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
