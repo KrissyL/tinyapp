dbf51815950550ca57713645bac3b6bd36968ce2
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-03 19:45:46 +0000

    created a delete function to delete urls

--- express_server.js
+++ express_server.js
@@ -31,10 +31,15 @@ app.post("/urls", (req, res) => {
     res.send("Ok");        // respond with "Ok" (tb replaced)
 });
 
+// delete a url
+app.post("/urls/:shortURL/delete", (req, res) => {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+});
+
 // make a route to handle shortURL reqs
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
-    console.log(req.params.shortURL);
     res.redirect(longURL);
 });
 
