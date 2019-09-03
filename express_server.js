7e5bae5174e7d3f0a73799b0970bef0bf2b1af46
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-03 00:49:28 +0000

    set express as view engine

--- express_server.js
+++ express_server.js
@@ -2,23 +2,5 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port
 
-const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
-
-app.get("/", (req, res) => {
-    res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// set ejs as view engine
+app.set("view engine", "ejs");
